08610ff5545592ed1de0f070973472921736fe85
Ievgenii Domanskyy <jb@div-art.com>
2018-02-02 14:15:10 +0200

    commit

--- src/components/main/PlayerCell.vue
+++ src/components/main/PlayerCell.vue
@@ -15,11 +15,6 @@
 
     export default {
         name: 'PlayerCell',
-        data() {
-            return {
-
-            }
-        },
         props: ['player', 'funcRemove'],
         methods: {
             choosePlayer(player) {
